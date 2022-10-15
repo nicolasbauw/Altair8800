7e8995bf622091ddf5e74dc3df7b61189d06ef09
Nicolas BAUW <nbauw@hotmail.com>
2022-10-15 10:31:55 +0200

    Menu is back

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::{ env, error::Error, process, thread, time ,time::Duration, io::Write, io::stdout};
+use std::{ env, error::Error, process, thread, time, io::Write, io::stdout, fs};
 use intel8080::CPU;
-use console::{Term, Key};
+use console::{Term, Key, style};
 
 fn main() {
     if let Err(e) = load_execute() {
@@ -89,7 +89,7 @@ fn load_execute() -> Result<(), Box<dyn Error>> {
     // Since the console crate read key function is blocking, we spawn a thread
     thread::spawn(move || {
         loop {
-            if let Some(ch) = getch(&term) {
+            if let Some(ch) = getch(&term, &tx) {
                 tx.send(ch).unwrap()
             }
         } 
@@ -102,13 +102,49 @@ fn load_execute() -> Result<(), Box<dyn Error>> {
 
 }
 
-fn getch(term: &console::Term) -> Option<u8> {
+fn getch(term: &console::Term, tx: &intel8080::crossbeam_channel::Sender<u8>) -> Option<u8> {
     match term.read_key() {
         Ok(k) => match k {
             Key::Char(c) => Some(c as u8),
             Key::Enter => Some(0x0d),
+            Key::Escape => {
+                if let Err(e) = toggle_menu(term, tx) { println!("{}", e) };
+                return None
+            },
             _ => None
         },
         Err(_) => None
+    }
+}
+
+fn toggle_menu(term: &console::Term, tx: &intel8080::crossbeam_channel::Sender<u8>) -> Result<(), Box<dyn Error>> {
+    let delay = time::Duration::from_millis(20);
+    term.move_cursor_to(0, 0)?;
+    term.clear_screen().unwrap();
+    println!("{}uit\t{}oad", style("[Q]").magenta(), style("[L]").magenta());
+    loop {
+        match term.read_key()? {
+            Key::Escape => { term.clear_screen().unwrap(); return Ok(())},
+            Key::Char('Q') => { process::exit(0) },
+            Key::Char('L') => {
+                term.clear_screen()?;
+                term.write_line("File ? ")?;
+                let file = term.read_line()?;
+                let bas = fs::read_to_string(file)?;
+                for line in bas.lines() {
+                    for c in line.chars() {
+                        tx.send(c as u8)?;
+                        thread::sleep(delay);
+                    }
+                    tx.send(0x0d)?;
+                    thread::sleep(delay*10);
+                }
+                return Ok(());
+            }
+            Key::Char('C') => {
+                tx.send(0x03)?;
+            }
+            _ => {}
+        }
     }
 }
