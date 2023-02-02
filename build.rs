9434b01ac1ce4848b1ef72508ba27f1eb5607f20
Nicolas BAUW <nbauw@hotmail.com>
2023-02-02 15:29:26 +0100

    Build script / config file

---
+++ build.rs
@@ -0,0 +1,20 @@
+use fs_extra::{copy_items, dir::create};
+use directories::UserDirs;
+
+fn main() {
+    if let Some(user_dirs) = UserDirs::new() {
+        let home_dir = user_dirs.home_dir();
+        let mut dest = home_dir.to_path_buf();
+        let options = fs_extra::dir::CopyOptions::new();
+
+        // Create teletype's config directory in user's home directory
+        dest.push(".config/teletype/");
+        create(&dest, true).unwrap();
+
+        // Copy teletype/config.toml to home_dir/.config/teletype/config.toml
+        let mut from_paths = Vec::new();
+        from_paths.push("config/config.toml");
+        println!("Copying {:#?} to {:#?}", from_paths, dest);
+        copy_items(&from_paths, dest, &options).unwrap(); 
+    }
+ }
