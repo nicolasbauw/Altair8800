7a19906b3e55bf72740092972c7532f6723ba322
Nicolas BAUW <nbauw@hotmail.com>
2023-08-06 19:00:39 +0200

    Back to non-channels IO

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::{ env, error::Error, process, thread, time, io::Write, io::stdout, fs};
-use intel8080::CPU;
+use std::{ env, error::Error, process, thread, sync::mpsc, io::stdout, io::Write, fs, time };
+use intel8080::{CPU, memory::ROMSpace};
 use console::{Term, Key, style};
 
 mod config;
@@ -12,14 +12,13 @@ fn main() {
 }
 
 fn load_execute() -> Result<(), Box<dyn Error>> {
-    let (tx, rx) = intel8080::crossbeam_channel::bounded::<u8>(1);
+    let (tx, rx) = mpsc::channel();
     let term = Term::stdout();
     let  mut a = env::args();
     let mut c = CPU::new();
-
     /* This byte of ROM at the end of address space is there to meet basic 3.2 initialization code requirement
     otherwise automatic RAM detection routine loops forever */
-    c.bus.set_romspace(0xffff, 0xffff);
+    c.bus.rom_space = Some(ROMSpace{start: 0xffff, end: 0xffff});
 
     // Loads assembled program into memory
     if let Some(f) = a.nth(1) {
@@ -29,64 +28,8 @@ fn load_execute() -> Result<(), Box<dyn Error>> {
         process::exit(1);
     }
 
-    let device0_req_receiver = c.bus.io_req.1.clone();
-    let device0_sender = c.bus.io_in.0.clone();
-
-    let altair_switches_req_receiver = c.bus.io_req.1.clone();
-    let altair_switches_sender = c.bus.io_in.0.clone();
-
-    let device1_sender = c.bus.io_in.0.clone();
-    let device1_receiver = c.bus.io_out.1.clone();
-
-    // Device 0 : teletype control channel
-    thread::spawn(move || {
-        loop {
-            // CPU ran an IN instruction ?
-            if let Ok(device) = device0_req_receiver.recv() {
-                // IN for device 0 ?
-                if device == 0 {
-                    match rx.try_recv() {
-                        // No key have been pressed ? device 0 sends 1
-                        Err(_) => {
-                            device0_sender.send((0,1)).unwrap();
-                        },
-                        // A key has been pressed ? device 0 sends 0 (output device ready)
-                        // Then, device 1 sends the key code
-                        Ok(ch) => {
-                            device0_sender.send((0,0)).unwrap();
-                            device1_sender.send((1,ch)).unwrap();
-                        }
-                    }
-                }
-            }
-        }
-    });
-
-    // Device 1 : send and receive ASCII data from the teletype
-    thread::spawn(move || {
-        loop {
-            if let Ok((device, data)) = device1_receiver.recv() {
-                // Device 1 received data ? Let's print it
-                if device == 1 {
-                    let value = data & 0x7f;
-                    if value >= 32 && value <=125 || value == 0x0a || value == 0x0d {
-                        print!("{}", value as char);
-                        stdout().flush().unwrap();
-                    }
-                }
-            }
-        }
-    });
-
-    // Device 255 : Altair switches
-    thread::spawn(move || {
-        if let Ok(device) = altair_switches_req_receiver.recv() {
-            if device == 255 {
-                // All switches down : that's the 88-SIO setting for MS Basic 3.2
-                altair_switches_sender.send((255, 0)).unwrap();
-            }
-        }
-    });
+    // Setting up Altair switches for 88-SIO (4K BASIC 3.2)
+    c.bus.set_io_in(255, 0x00);
 
     // Since the console crate read key function is blocking, we spawn a thread
     thread::spawn(move || {
@@ -97,14 +40,32 @@ fn load_execute() -> Result<(), Box<dyn Error>> {
         } 
     });
 
-    // CPU loop
     loop {
         c.execute_slice();
-    }
 
+        // Will likely never happen. There just to meet function return type requirement.
+        if c.pc == 0xffff { return Ok(()) };
+
+        if let Ok(ch) = rx.try_recv() {
+            c.bus.set_io_in(0, 0);
+            c.bus.set_io_in(1, ch);
+        }
+    
+        // Data sent to device 1 (OUT) ? we display it
+        if let Some(v) = c.bus.get_io_out(1) {
+            let value = v & 0x7f;
+            if value >= 32 && value <=125 || value == 0x0a || value == 0x0d {
+                print!("{}", value as char);
+                stdout().flush()?;
+                // Clearing IO (in and out) to be ready for next key press
+                c.bus.clear_io_out();
+                c.bus.set_io_in(0, 1);
+            }
+        }
+    }
 }
 
-fn getch(term: &console::Term, tx: &intel8080::crossbeam_channel::Sender<u8>) -> Option<u8> {
+fn getch(term: &console::Term, tx: &std::sync::mpsc::Sender<u8>) -> Option<u8> {
     match term.read_key() {
         Ok(k) => match k {
             Key::Char(c) => Some(c as u8),
@@ -119,7 +80,7 @@ fn getch(term: &console::Term, tx: &intel8080::crossbeam_channel::Sender<u8>) ->
     }
 }
 
-fn toggle_menu(term: &console::Term, tx: &intel8080::crossbeam_channel::Sender<u8>) -> Result<(), Box<dyn Error>> {
+fn toggle_menu(term: &console::Term, tx: &std::sync::mpsc::Sender<u8>) -> Result<(), Box<dyn Error>> {
     let config = config::load_config_file()?;
     term.move_cursor_to(0, 0)?;
     term.clear_screen().unwrap();
