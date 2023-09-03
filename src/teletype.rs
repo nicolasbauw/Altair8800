bb06a0a244aa6fe58486072c1e4ae23c57eee3de
Nicolas BAUW <nbauw@hotmail.com>
2023-09-03 20:43:52 +0200

    WIP IO / restructuration

---
+++ src/teletype.rs
@@ -0,0 +1,154 @@
+use crate::config;
+use console::{style, Key, Term};
+use intel8080::CPU;
+use std::{
+    env, error::Error, fs, io::stdout, io::Write, process, sync::mpsc, thread, time::Duration,
+};
+
+struct Teletype {
+    control: u8, // Device 0
+    data: u8,    // Device 1
+}
+
+impl Teletype {
+    fn new() -> Self {
+        Self {
+            control: 0,
+            data: 0,
+        }
+    }
+}
+
+pub fn run() -> Result<(), Box<dyn Error>> {
+    let (tx, rx) = mpsc::channel();
+    let term = Term::stdout();
+    let mut a = env::args();
+    let mut c = CPU::new();
+    /* This byte of ROM at the end of address space is there to meet basic 3.2 initialization code requirement
+    otherwise automatic RAM detection routine loops forever */
+    c.bus.set_romspace(0xffff, 0xffff);
+
+    // Loads assembled program into memory
+    if let Some(f) = a.nth(1) {
+        c.bus.load_bin(&f, 0x0)?;
+    } else {
+        println!("No file specified");
+        process::exit(1);
+    }
+
+    let mut teletype = Teletype::new();
+
+    // Since the console crate read key function is blocking, we spawn a thread
+    thread::spawn(move || loop {
+        if let Some(ch) = getch(&term, &tx) {
+            tx.send(ch).unwrap()
+        }
+    });
+
+    loop {
+        // Checking data sent from the "teletype" thread
+        if let Ok(ch) = rx.try_recv() {
+            // A key has been pressed ? device 0 sends 0 (output device ready)
+            teletype.control = 0;
+            // Then, device 1 sends the key code
+            teletype.data = ch;
+        }
+
+        // Will likely never happen. There just to meet function return type requirement.
+        if c.pc == 0xffff {
+            return Ok(());
+        };
+
+        let pc = c.pc;
+        let opcode = c.bus.read_byte(pc);
+        match opcode {
+            // IN
+            0xdb => {
+                let port = c.bus.read_byte(pc + 1);
+                c.reg.a = match port {
+                    0xFF => 0, // Altair switches set up for 88-SIO (4K BASIC 3.2)
+                    0x00 => teletype.control,
+                    0x01 => teletype.data,
+                    _ => c.reg.a,
+                }
+            }
+            // OUT
+            0xd3 => {
+                let port = c.bus.read_byte(pc + 1);
+                if port == 0x01 {
+                    let value = c.reg.a & 0x7f;
+                    if value >= 32 && value <= 125 || value == 0x0a || value == 0x0d {
+                        print!("{}", value as char);
+                        stdout().flush()?;
+                    }
+                }
+                // Reset teletype control register
+                teletype.control = 1;
+            }
+            _ => {}
+        }
+
+        if let Some(t) = c.execute_timed() {
+            thread::sleep(Duration::from_millis(t.into()))
+        }
+    }
+}
+
+fn getch(term: &console::Term, tx: &std::sync::mpsc::Sender<u8>) -> Option<u8> {
+    match term.read_key() {
+        Ok(k) => match k {
+            Key::Char(c) => Some(c as u8),
+            Key::Enter => Some(0x0d),
+            Key::Escape => {
+                if let Err(e) = toggle_menu(term, tx) {
+                    println!("{}", e)
+                };
+                return None;
+            }
+            _ => None,
+        },
+        Err(_) => None,
+    }
+}
+
+fn toggle_menu(
+    term: &console::Term,
+    tx: &std::sync::mpsc::Sender<u8>,
+) -> Result<(), Box<dyn Error>> {
+    let config = config::load_config_file()?;
+    term.move_cursor_to(0, 0)?;
+    term.clear_screen().unwrap();
+    println!(
+        "{}uit\t{}oad",
+        style("[Q]").magenta(),
+        style("[L]").magenta()
+    );
+    loop {
+        match term.read_key()? {
+            Key::Escape => {
+                term.clear_screen().unwrap();
+                return Ok(());
+            }
+            Key::Char('Q') => process::exit(0),
+            Key::Char('L') => {
+                term.clear_screen()?;
+                term.write_line("File ? ")?;
+                let file = term.read_line()?;
+                let bas = fs::read_to_string(file)?;
+                for line in bas.lines() {
+                    for c in line.chars() {
+                        tx.send(c as u8)?;
+                        thread::sleep(std::time::Duration::from_millis(config.keyboard.char_delay));
+                    }
+                    tx.send(0x0d)?;
+                    thread::sleep(std::time::Duration::from_millis(config.keyboard.line_delay));
+                }
+                return Ok(());
+            }
+            Key::Char('C') => {
+                tx.send(0x03)?;
+            }
+            _ => {}
+        }
+    }
+}
