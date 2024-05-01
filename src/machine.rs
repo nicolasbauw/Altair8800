28f95d5170a854aef8a38c4b18e43db32c5b0f0b
Nicolas BAUW <nbauw@hotmail.com>
2024-05-01 18:29:22 +0200

    Machine struct

--- src/machine.rs
+++ src/machine.rs
@@ -2,77 +2,86 @@ use console::Term;
 use intel8080::cpu::CPU;
 use std::{env, error::Error, io::stdout, io::Write, process, sync::mpsc, thread, time::Duration};
 
-pub fn run() -> Result<(), Box<dyn Error>> {
-    let (tx, rx) = mpsc::channel();
-    let term = Term::stdout();
-    let mut a = env::args();
-    let mut c = CPU::new();
-    /* This byte of ROM at the end of address space is there to meet basic 3.2 initialization code requirement
-    otherwise automatic RAM detection routine loops forever */
-    c.bus.set_romspace(0xffff, 0xffff);
+pub struct Machine {
+    pub cpu: CPU,
+}
 
-    // Loads assembled program into memory
-    if let Some(f) = a.nth(1) {
-        c.bus.load_bin(&f, 0x0)?;
-    } else {
-        println!("No file specified");
-        process::exit(1);
+impl Machine {
+    pub fn new() -> Self {
+        Self { cpu: CPU::new() }
     }
 
-    let mut teletype = crate::teletype::Teletype::new();
+    pub fn run(&mut self) -> Result<(), Box<dyn Error>> {
+        let (tx, rx) = mpsc::channel();
+        let mut a = env::args();
+        let term = Term::stdout();
+        /* This byte of ROM at the end of address space is there to meet basic 3.2 initialization code requirement
+        otherwise automatic RAM detection routine loops forever */
+        self.cpu.bus.set_romspace(0xffff, 0xffff);
 
-    // Since the console crate read key function is blocking, we spawn a thread
-    thread::spawn(move || loop {
-        if let Some(ch) = crate::teletype::getch(&term, &tx) {
-            tx.send(ch).unwrap()
-        }
-    });
-
-    loop {
-        // Checking if data was sent from the "teletype" thread
-        if let Ok(ch) = rx.try_recv() {
-            // A key has been pressed ? device 0 sends 0 (output device ready)
-            teletype.control = 0;
-            // Then, device 1 sends the key code
-            teletype.data = ch;
+        // Loads assembled program into memory
+        if let Some(f) = a.nth(1) {
+            self.cpu.bus.load_bin(&f, 0x0)?;
+        } else {
+            println!("No file specified");
+            process::exit(1);
         }
 
-        // Will likely never happen. There just to meet function return type requirement.
-        if c.pc == 0xffff {
-            return Ok(());
-        };
+        let mut teletype = crate::teletype::Teletype::new();
 
-        let pc = c.pc;
-        let opcode = c.bus.read_byte(pc);
-        match opcode {
-            // IN
-            0xdb => {
-                let port = c.bus.read_byte(pc + 1);
-                c.reg.a = match port {
-                    0xFF => 0, // Altair switches set up for 88-SIO (4K BASIC 3.2)
-                    0x00 => teletype.control,
-                    0x01 => teletype.data,
-                    _ => c.reg.a,
-                }
+        // Since the console crate read key function is blocking, we spawn a thread
+        thread::spawn(move || loop {
+            if let Some(ch) = crate::teletype::getch(&term, &tx) {
+                tx.send(ch).unwrap()
             }
-            // OUT
-            0xd3 => {
-                let port = c.bus.read_byte(pc + 1);
-                if port == 0x01 {
-                    let value = c.reg.a & 0x7f;
-                    if value >= 32 && value <= 125 || value == 0x0a || value == 0x0d {
-                        print!("{}", value as char);
-                        stdout().flush()?;
+        });
+
+        loop {
+            // Checking if data was sent from the "teletype" thread
+            if let Ok(ch) = rx.try_recv() {
+                // A key has been pressed ? device 0 sends 0 (output device ready)
+                teletype.control = 0;
+                // Then, device 1 sends the key code
+                teletype.data = ch;
+            }
+
+            // Will likely never happen. There just to meet function return type requirement.
+            if self.cpu.pc == 0xffff {
+                return Ok(());
+            };
+
+            let pc = self.cpu.pc;
+            let opcode = self.cpu.bus.read_byte(pc);
+            match opcode {
+                // IN
+                0xdb => {
+                    let port = self.cpu.bus.read_byte(pc + 1);
+                    self.cpu.reg.a = match port {
+                        0xFF => 0, // Altair switches set up for 88-SIO (4K BASIC 3.2)
+                        0x00 => teletype.control,
+                        0x01 => teletype.data,
+                        _ => self.cpu.reg.a,
                     }
                 }
-                // Reset teletype control register
-                teletype.control = 1;
+                // OUT
+                0xd3 => {
+                    let port = self.cpu.bus.read_byte(pc + 1);
+                    if port == 0x01 {
+                        let value = self.cpu.reg.a & 0x7f;
+                        if value >= 32 && value <= 125 || value == 0x0a || value == 0x0d {
+                            print!("{}", value as char);
+                            stdout().flush()?;
+                        }
+                    }
+                    // Reset teletype control register
+                    teletype.control = 1;
+                }
+                _ => {}
             }
-            _ => {}
-        }
 
-        if let Some(t) = c.execute_timed() {
-            thread::sleep(Duration::from_millis(t.into()))
+            if let Some(t) = self.cpu.execute_timed() {
+                thread::sleep(Duration::from_millis(t.into()))
+            }
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,11 @@ mod config;
 mod machine;
 mod teletype;
 
+use machine::Machine;
+
 fn main() {
-    if let Err(e) = machine::run() {
+    let mut m = Machine::new();
+    if let Err(e) = m.run() {
         println!("{}", e);
         process::exit(1);
     }
