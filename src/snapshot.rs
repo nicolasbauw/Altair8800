eb71aa26bcc73fbdcf5ecf076fd431144093426b
Nicolas BAUW <nbauw@hotmail.com>
2024-05-09 20:12:53 +0200

    SnapshotError moved to intel8080 crate

--- src/snapshot.rs
+++ src/snapshot.rs
@@ -1,30 +1,6 @@
 use crate::Machine;
-use std::{error, fmt, fs, path::PathBuf};
-
-#[derive(Debug, PartialEq, Eq, Clone)]
-pub enum SnapshotError {
-    InvalidHeader,
-    IOError,
-}
-
-impl fmt::Display for SnapshotError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.write_str("Snapshot error : ")?;
-        f.write_str(match self {
-            SnapshotError::InvalidHeader => "Invalid header",
-            SnapshotError::IOError => "I/O Error",
-        })
-    }
-}
-
-impl From<std::io::Error> for SnapshotError {
-    fn from(_e: std::io::Error) -> SnapshotError {
-        SnapshotError::IOError
-    }
-}
-
-impl error::Error for SnapshotError {}
-
+use intel8080::memory::SnapshotError;
+use std::{fs, path::PathBuf};
 impl Machine {
     pub fn save_snapshot(&mut self) -> std::io::Result<()> {
         // We create the snapshot file
