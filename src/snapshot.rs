0b2964656fa0c22ce83035b2d8a3edfa28bdf504
Nicolas BAUW <nbauw@hotmail.com>
2024-05-09 20:29:17 +0200

    TODO error conversion

--- src/snapshot.rs
+++ src/snapshot.rs
@@ -14,16 +14,12 @@ impl Machine {
         Ok(())
     }
 
-    pub fn load_snapshot(&mut self) -> Result<(), SnapshotError> {
+    pub fn load_snapshot(&mut self) -> std::io::Result<()> {
         let mut file = PathBuf::from(&self.config.snapshot.dir);
         file.push("test.snapshot");
 
         let snapshot = fs::read(file)?;
-        if snapshot[0..=3] != [0x41, 0x4c, 0x54, 0x52] {
-            return Err(SnapshotError::InvalidHeader);
-        }
-
-        self.cpu.import_snapshot(snapshot);
+        self.cpu.import_snapshot(snapshot)?;
 
         Ok(())
     }
